5c8ada5c48c2ae89946637263a4a1d60f20aaf74
84000-admin <dominic.latham@84000.co>
2024-06-27 13:47:09 +0200

    part rewrite commented back in

--- next.config.mjs
+++ next.config.mjs
@@ -12,10 +12,10 @@ const nextConfig = {
   },
   async rewrites() {
     return [
-      // {
-      //   source: '/translation/:path/:id',
-      //   destination: '/translation/:path/:id.html',
-      // },
+      {
+         source: '/translation/:path*/:id',
+         destination: 'https://data-static.vercel.app/translation/:path/:id.html',
+      },
       {
         source: '/source/:path*/folio/:id',
         destination: 'https://data-static.vercel.app/source/:path*/folio-:id.html',
